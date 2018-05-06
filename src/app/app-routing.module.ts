d147946e51a861d6aa6551d47bf962f62f6e1cdf
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-05-05 21:51:35 -0500

    fixed footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -67,7 +67,7 @@ const routes: Routes =
   { path: '**', component: HomeComponent }
 ];
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [ RouterModule.forRoot(routes, {useHash: true}) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
